fa45325e6ee05dc8acbbde5082e02895239d9005
enzo <enzocarnevali708@gmail.com>
2023-08-27 14:37:11 -0300

    fmt

--- variables.tf
+++ variables.tf
@@ -1,16 +1,16 @@
 variable "admin_password" {
-  type = string
+  type        = string
   description = "Password used to connect to the machine via SSH"
 }
 
-variable "admin_username"{
-    type = string
-    description = "Name of the admin user of the machine"
-    default = "minecraft"
+variable "admin_username" {
+  type        = string
+  description = "Name of the admin user of the machine"
+  default     = "minecraft"
 }
 
 variable "machine_size" {
-    type = string
-    description = "Size of the machine hosting the minecraft server"
-    default = "Standard_B2s"
+  type        = string
+  description = "Size of the machine hosting the minecraft server"
+  default     = "Standard_B2s"
 }
