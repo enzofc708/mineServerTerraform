ec60df44a8d97e9fb0746ff189755878e31f2e68
enzo <enzocarnevali708@gmail.com>
2023-08-27 14:57:49 -0300

    refactor module

--- main.tf
+++ main.tf
@@ -1,25 +1,95 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "=3.0.0"
-    }
-  }
-
-  cloud {
-    organization = "enzofc"
-
-    workspaces {
-      name = "CLI"
-    }
-  }
-}
-
-provider "azurerm" {
-  features {}
-}
+################################################################################
+# Resource Group
+################################################################################
 
 resource "azurerm_resource_group" "resourcegroup" {
   name     = "minecraftRG"
   location = "Brazil South"
+}
+
+################################################################################
+# Network Resources
+################################################################################
+
+resource "azurerm_virtual_network" "network" {
+  name                = "network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+}
+
+resource "azurerm_network_security_group" "secGroup" {
+  name                = "secGroup"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 1001
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "internal"
+  resource_group_name  = azurerm_resource_group.resourcegroup.name
+  virtual_network_name = azurerm_virtual_network.network.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_public_ip" "mineIP" {
+  name                = "minecraftServerIP"
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  location            = azurerm_resource_group.resourcegroup.location
+  allocation_method   = "Static"
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "nic"
+  location            = azurerm_resource_group.resourcegroup.location
+  resource_group_name = azurerm_resource_group.resourcegroup.name
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.mineIP.id
+  }
+}
+
+resource "azurerm_network_interface_security_group_association" "example" {
+  network_interface_id      = azurerm_network_interface.nic.id
+  network_security_group_id = azurerm_network_security_group.secGroup.id
+}
+
+################################################################################
+# Virtual Machines
+################################################################################
+
+resource "azurerm_linux_virtual_machine" "mineServer" {
+  name                            = "minecraftServer"
+  admin_username                  = var.admin_username
+  admin_password                  = var.admin_password
+  disable_password_authentication = false
+  location                        = azurerm_resource_group.resourcegroup.location
+  network_interface_ids           = [azurerm_network_interface.nic.id]
+  resource_group_name             = azurerm_resource_group.resourcegroup.name
+  size                            = var.machine_size
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+
+  source_image_reference {
+    publisher = "Canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts-gen2"
+    version   = "latest"
+  }
 }
